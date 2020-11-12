77e272ebcf59e85c9d02111422bdc814b6cacbb9
Richard <chownv@hotmail.com>
2020-11-11 20:33:37 -0800

    second fragment

--- weFit/app/src/main/java/com/example/wefit/SecondFragment.java
+++ weFit/app/src/main/java/com/example/wefit/SecondFragment.java
@@ -5,17 +5,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class SecondFragment extends Fragment implements View.OnClickListener {
 
     private Button activityButton;
-    private Button SecondButton;
+
+
+    private RecyclerView mRecyclerView;
+
+
+
 
     @Override
     public View onCreateView(
@@ -30,32 +40,50 @@ public class SecondFragment extends Fragment implements View.OnClickListener {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-//        SecondButton= view.findViewById(R.id.button_second);
-//        SecondButton.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                if(firstFragment==null){
-//                    firstFragment = new FirstFragment();
-//                }
-//                getFragmentManager().beginTransaction().replace(R.id.activity_window,firstFragment).commitAllowingStateLoss();
-//
-//            }
-//
-//
-//
-//        });
 
 
-//        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                NavHostFragment.findNavController(SecondFragment.this)
-//                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
 //
-//            }
-//        });
 
+    }
+    public void onStart() {
+        super.onStart();
 
+    }
+    private class VH extends RecyclerView.ViewHolder {
+        TextView tv1;
+        TextView tv2;
+
+        public VH(@NonNull View itemView) {
+            super(itemView);
+            tv1 = itemView.findViewById(R.id.tv1);
+            tv2 = itemView.findViewById(R.id.tv2);
+        }
+    }
+    private class LetterAdapter extends RecyclerView.Adapter<VH> {
+
+        private List<Character> dataList;
+
+        public LetterAdapter(List<Character> dataList) {
+            this.dataList = dataList;
+        }
+
+        @NonNull
+        @Override
+        public VH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new VH(LayoutInflater.from(parent.getContext()).inflate(R.layout.header, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull VH holder, int position) {
+            Character c = dataList.get(position);
+            holder.tv1.setText(c.toString());
+            holder.tv2.setText(String.valueOf(Integer.valueOf(c)));
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataList.size();
+        }
     }
 
 
