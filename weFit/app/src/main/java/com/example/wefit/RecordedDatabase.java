64d716369a7d66b58c8b4d42cdd472e87f17dbbe
A <laz>
2020-11-17 00:38:34 -0800

    add query

--- weFit/app/src/main/java/com/example/wefit/RecordedDatabase.java
+++ weFit/app/src/main/java/com/example/wefit/RecordedDatabase.java
@@ -35,5 +35,38 @@ public class RecordedDatabase {
         return cursor;
     }
 
+    public int deleRow(String item){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String[] deteleItem = {item};
+        int count = db.delete(Constants.TABLE_NAME, Constants.TYPE + "=?", deteleItem);
+        return count;
+    }
+
+    public String getSelectedType(String type){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String[] columns = {Constants.UID, Constants.TYPE, Constants.DISTANCE, Constants.TIME, Constants.SPEED, Constants.CALORY};
+
+        String selection = Constants.TYPE + "='" + type + "'";
+        Cursor cursor = db.query(Constants.TABLE_NAME, columns, selection,null, null, null, null);
+
+        StringBuffer buffer = new StringBuffer();
+        while(cursor.moveToNext()){
+            int index1 = cursor.getColumnIndex(Constants.TYPE);
+            int index2 = cursor.getColumnIndex(Constants.DISTANCE);
+            int index3 = cursor.getColumnIndex(Constants.TIME);
+            int index4 = cursor.getColumnIndex(Constants.SPEED);
+            int index5 = cursor.getColumnIndex(Constants.CALORY);
+            String typeA = cursor.getString(index1);
+            String dist = cursor.getString(index2);
+            String time = cursor.getString(index3);
+            String speed = cursor.getString(index4);
+            String claory = cursor.getString(index5);
+            buffer.append(typeA + " " +  dist + " " + time + " " + speed + " " + claory + "\n");
+
+
+        }
+        return  buffer.toString();
+    }
+
 
 }
